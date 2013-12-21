06da9a6167708bbc3d0de3704027730b391ca267
myun2 <myun2@nwhite.info>
2013-12-21 14:41:22 +0000

    init project (gojo-proto)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GojoProto::Application.config.secret_key_base = '054ed2626f302f36d0ef06d24fc144de0aeed7538a89dcc370d11725f3db2b4a58a83bddee8f4cfb03e52341d1b3954500d6fe8748fe8dcaaec77e4e6caa4139'
