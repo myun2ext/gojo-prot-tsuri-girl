294eba3c013f72aa19f00f231a1a959fd68f2406
myun2 <myun2@nwhite.info>
2014-03-10 16:36:07 +0900

    add recipe_images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310061445) do
+ActiveRecord::Schema.define(version: 20140310073537) do
 
   create_table "entried_positions", force: true do |t|
     t.integer  "entry_id"
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 20140310061445) do
     t.text     "howtomake"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: true do |t|
