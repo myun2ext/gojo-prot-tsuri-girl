f24d6adc8359a56f74d92d756e71dd44527caf0a
myun2 <myun2@nwhite.info>
2014-03-10 17:30:51 +0900

    Entry's image upload

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,13 +2,22 @@ class Entry < ActiveRecord::Base
   has_one :entried_position
   has_one :entried, as: :entried_position  # aliae
   has_one :position, as: :entried_position # alias
+  has_many :entry_images
+  has_many :images, as: :entry_images
 
   delegate :latitude, :latitude=, to: :entried_position, allow_nil: true
   delegate :longitude, :longitude=, to: :entried_position, allow_nil: true
 
   def self.new(attributes = nil, options = {})
     new_entry = super(attributes, options)
-    new_entry.entried_position = EntriedPosition.new
     new_entry
+  end
+
+  def image
+    self.entry_images.first.try(:image)
+  end
+
+  def image=(image)
+    self.entry_images = [EntryImage.new(image: image)]
   end
 end
