9687d87f03ef0d47a1731f4b216e03e6892d0cb6
myun2 <myun2@nwhite.info>
2014-03-10 15:22:53 +0900

    Entry: EntriedPosition assosiations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,14 @@
 class Entry < ActiveRecord::Base
+  has_one :entried_position
+  has_one :entried, as: :entried_position  # aliae
+  has_one :position, as: :entried_position # alias
+
+  delegate :latitude, to: :entried_position
+  delegate :longitude, to: :entried_position
+
+  def self.new(attributes = nil, options = {})
+    new_entry = super(attributes, options)
+    new_entry.entried_position = EntriedPosition.new
+    new_entry
+  end
 end
